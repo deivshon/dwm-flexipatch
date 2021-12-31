7b4ab993496796a8f8c6aab67fba4081ca20a7a5
Davide Cioni <davide.cioni.128@gmail.com>
2021-12-31 18:44:21 +0100

    Change bar font size

--- config.h
+++ config.h
@@ -127,9 +127,9 @@ static void (*bartabmonfns[])(Monitor *) = { NULL /* , customlayoutfn */ };
 #if BAR_PANGO_PATCH
 static const char font[]                 = "monospace 10";
 #else
-static const char *fonts[]               = { "monospace:size=12" };
+static const char *fonts[]               = { "monospace:size=14" };
 #endif // BAR_PANGO_PATCH
-static const char dmenufont[]            = "monospace:size=12";
+static const char dmenufont[]            = "monospace:size=14";
 
 static char c000000[]                    = "#000000"; // placeholder value
 

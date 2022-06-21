100351afbc79fb18b1c9df63912a9405abbb1ad8
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-06-21 14:34:28 +0200

    Remove GIMP and Firefox special rules

--- config.h
+++ config.h
@@ -141,8 +141,8 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
+	// RULE(.class = "Gimp", .tags = 1 << 4)
+	// RULE(.class = "Firefox", .tags = 1 << 7)
 };
 
 

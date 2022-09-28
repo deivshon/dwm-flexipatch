76661000a1eec8e3bf7b29f1da22e2e18b615ee0
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-09-28 13:04:07 +0200

    Remove noborders patch and actually add no transparent borders patch

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappih         = 5;   /* horiz inner gap between windo
 static const unsigned int gappiv         = 5;   /* vert inner gap between windows */
 static const unsigned int gappoh         = 7;   /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 7;   /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 0;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 
@@ -17,7 +17,6 @@ static const int topbar                  = 1;   /* 0 means bottom bar */
 static const int statusmon               = 'A';
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int showsystray             = 1;   /* 0 means no systray */
-
 
 /* Indicators: see patch/bar_indicators.h for options */
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
@@ -270,7 +269,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                                  8)
 };
 
-
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
@@ -287,5 +285,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,              Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,              Button3,        toggletag,      {0} },
 };
-
-
